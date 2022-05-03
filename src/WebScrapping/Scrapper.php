c28740996ff2d19ceb59d5e4e7dd42030c13b426
Taíssa Silva <61012521+sTaissa@users.noreply.github.com>
2022-05-03 16:03:23 -0300

    adiciona titulos dos trabalhos na planilha - webscrapping

--- src/WebScrapping/Scrapper.php
+++ src/WebScrapping/Scrapper.php
@@ -2,6 +2,10 @@
 
 namespace Galoa\ExerciciosPhp2022\WebScrapping;
 
+use DOMXPath;
+use Box\Spout\Writer\Common\Creator\WriterEntityFactory;
+use Box\Spout\Common\Entity\Row;
+
 /**
  * Does the scrapping of a webpage.
  */
@@ -11,7 +15,33 @@ class Scrapper {
    * Loads paper information from the HTML and creates a XLSX file.
    */
   public function scrap(\DOMDocument $dom): void {
-    print $dom->saveHTML();
+    $writer = WriterEntityFactory::createXLSXWriter();
+    $writer->openToFile(__DIR__ . '/../../webscrapping/result.xlsx');
+
+    /** 
+     * Find each article with xPath 
+     * */
+    $xPath = new DomXpath($dom);
+    $domNodeList = $xPath->query('.//a[@class="paper-card p-lg bd-gradient-left"]');
+
+    /** 
+     * Adds each element of the article to a worksheet cell 
+     * */
+    foreach($domNodeList as $article){
+      $cells = [];
+      $child = $article->childNodes;
+
+      foreach($child as $childElement){
+        if($childElement->nodeName == "h4"){
+          $title = $childElement->textContent;
+          
+          array_push($cells, WriterEntityFactory::createCell($title));
+        }
+      }
+      $singleRow = WriterEntityFactory::createRow($cells);
+      $writer->addRow($singleRow);
+    }
+    $writer->close();
   }
 
 }
