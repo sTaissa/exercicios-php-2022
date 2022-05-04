7579f88e11aa6f4a6484210e97321b9fc18ca8dc
Taíssa Silva <61012521+sTaissa@users.noreply.github.com>
2022-05-03 21:26:05 -0300

    adiona dados na planilha - webscrapping

--- src/WebScrapping/Scrapper.php
+++ src/WebScrapping/Scrapper.php
@@ -16,28 +16,37 @@ class Scrapper {
    */
   public function scrap(\DOMDocument $dom): void {
     $writer = WriterEntityFactory::createXLSXWriter();
-    $writer->openToFile(__DIR__ . '/../../webscrapping/result.xlsx');
+    $writer->openToFile(__DIR__ . '/../../webscrapping/model.xlsx');
 
-    /** 
-     * Find each article with xPath 
-     * */
+    //Find each article with xPath 
     $xPath = new DomXpath($dom);
     $domNodeList = $xPath->query('.//a[@class="paper-card p-lg bd-gradient-left"]');
 
-    /** 
-     * Adds each element of the article to a worksheet cell 
-     * */
+    //Adds each element of the article to a worksheet cell 
     foreach($domNodeList as $article){
-      $cells = [];
-      $child = $article->childNodes;
+      $authors = [];
 
-      foreach($child as $childElement){
-        if($childElement->nodeName == "h4"){
-          $title = $childElement->textContent;
-          
-          array_push($cells, WriterEntityFactory::createCell($title));
+      $id = $article->childNodes[2]->childNodes[1]->nodeValue;
+      $title = $article->childNodes[0]->nodeValue;
+      $type = $article->childNodes[2]->childNodes[0]->nodeValue;
+
+      foreach($article->childNodes[1]->childNodes as $span){
+        if($span->nodeType == 1){
+          array_push($authors, array($span->nodeValue, $span->getAttribute("title")));
         }
       }
+
+      $cells = [
+        WriterEntityFactory::createCell($id),
+        WriterEntityFactory::createCell($title),
+        WriterEntityFactory::createCell($type)
+      ];
+
+      for($i=0; $i<count($authors); $i++){
+        array_push($cells, WriterEntityFactory::createCell($authors[$i][0]));
+        array_push($cells, WriterEntityFactory::createCell($authors[$i][1]));
+      }
+
       $singleRow = WriterEntityFactory::createRow($cells);
       $writer->addRow($singleRow);
     }
